183df48d895677f8c983b2e65285c899953e1ece
VinayakKesarwani <97671264+VinayakKesarwani@users.noreply.github.com>
2023-01-15 00:12:00 +0530

    Updated post from sign in page

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+// const alert = require('alert');
+// var swal = require('sweetalert');
+// const popup = require('node-popup');
+// const popup = require('node-popup/dist/cjs.js');
+// var jsdom = require("jsdom");
+// var JSDOM = jsdom.JSDOM;
+// global.document = new JSDOM(html).window.document;
 
 const app = express();
 app.use(bodyParser.urlencoded({extended:true}))
@@ -21,17 +28,39 @@ app.get('/register', function(req, res){
 });
 
 app.post('/register', (req, res) => {
-  const user = new User({
-    email: req.body.email,
-    password: req.body.password
+  User.findOne({email: req.body.email}, (err, userAlready) => {
+    if(userAlready){
+      res.redirect('/register');
+    }
+    else{
+      const user = new User({
+        email: req.body.email,
+        password: req.body.password
+      });
+      user.save()
+        .then(() => {
+          res.status(201).json({ message: 'User created successfully!' });
+        })
+        .catch(error => {
+          res.status(400).json({ error });
+        });
+    }
   });
-  user.save()
-    .then(() => {
-      res.status(201).json({ message: 'User created successfully!' });
-    })
-    .catch(error => {
-      res.status(400).json({ error });
-    });
+});
+
+app.get('/signin', function(req, res){
+  res.sendFile(__dirname + '/signin.html');
+});
+
+app.post('/signin', function(req, res){
+  User.findOne({ $and: [{ email: req.body.email }, { password: req.body.password }] }, (err, foundedUser) => {
+  if (err) throw err;
+  if (foundedUser) {
+    res.sendFile(__dirname + '/home.html')
+  } else {
+    res.redirect('/signin')
+  }
+});
 });
 
 app.listen(3000, () => {
